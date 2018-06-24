be8390bc2f6c6eb9fa3457508b3252327a70050f
623583242 <39154790+623583242@users.noreply.github.com>
2018-06-24 20:08:35 +0800

    Add files via upload

---
+++ Tool.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "cocos2d.h"
+#include "iconv.h"
+#include <iostream>
+#include <string.h>
+int code_convert(const char *from_charset, const char *to_charset, const char *inbuf, size_t inlen, char *outbuf, size_t outlen);
+ 
+std::string u2a(const char *inbuf);
+  
+std::string a2u(const char *inbuf);
